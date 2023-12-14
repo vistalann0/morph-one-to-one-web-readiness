16ebf224d8e59c6a6eb4397b8ac7fe7c8f2088f1
vistalann0 <146479343+vistalann0@users.noreply.github.com>
2023-12-14 19:00:31 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,53 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    data = generate_random_data()
+    main()
+
+    return data
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+if __name__ == "__main__":
+import random
+    for item in data:
+
+
+
+    for item in data:
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def generate_random_data():
+    main()
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+import random
+if __name__ == "__main__":
+
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+import random
+    for item in data:
+def main():
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+    main()
+    return data
+    data = generate_random_data()
+def generate_random_data():
+        print(f"Random Number: {item}")
+import random
+
+    main()
+def main():
+
+
+if __name__ == "__main__":
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
